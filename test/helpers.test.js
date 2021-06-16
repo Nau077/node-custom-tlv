43fb7754a2e7ad3915e2ea57b0474ef83599c575
Nau077 <antbobrikov@yandex.ru>
2021-06-16 23:38:43 +0300

    cover 94% by tests

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -5,8 +5,7 @@ const {
     deleteWhiteSpaces,
     createLengthParams,
     makeToUpperCase,
-    computeCustomByteLengths,
-    BYTE_FACTOR
+    computeCustomByteLengths
   } = require("../lib/helpers/helpers")
 
   describe('#Helpers', function() {
@@ -20,8 +19,6 @@ const {
 			baseTagLength: 2,
 			baseLengthTlvLength: 1
 			};
-
- 
 
 	 	expect(validateArgs(settings)).to.deep.equal(!IS_VALID);
  
@@ -42,6 +39,60 @@ const {
             expect(validateArgs(settings)).to.deep.equal(!IS_VALID);
      
             });
+
+        it('should set empty string settings', () => {
+        
+            let settings = ""
+
+            expect(validateArgs(settings)).to.deep.equal(!IS_VALID);
+
+            settings = null
+
+            expect(validateArgs(settings)).to.deep.equal(!IS_VALID);
+
+            settings = []
+
+            expect(validateArgs(settings)).to.deep.equal(!IS_VALID);
+        
+            });
+
+        it('should set not number fields', () => {
+
+        let settings = 	{
+            dictionary: tagDictionary,
+            baseTagLength: "kdkdk",
+            baseLengthTlvLength: "jddjdj"
+            };
+
+        expect(validateArgs(settings)).to.deep.equal(!IS_VALID);
+
+
+        });
+
+        it('should set not correct dictionary', () => {
+
+        let settings = 	{
+            dictionary: {
+             '0002': { name2: 'messageId' },
+             '0004': { nasame: 'commandCode' },
+            },
+			baseTagLength: 2,
+			baseLengthTlvLength: 1
+            };
+
+        expect(validateArgs(settings)).to.deep.equal(!IS_VALID);
+
+        settings = 	{
+            dictionary: {
+             '0002': "dldldl"
+            },
+			baseTagLength: 2,
+			baseLengthTlvLength: 1
+            };
+
+        expect(validateArgs(settings)).to.deep.equal(!IS_VALID);
+        });
+
 
         it('should set correct settings', () => {
         
@@ -102,6 +153,93 @@ const {
 
                 expect(result).to.deep.equal(JSON.stringify(expected));
             });
+
+            it('should   create lengths params without customByteLengthComputed', () => {
+                const payload = {
+                    baselengthByte: '01',
+                    i: 10,
+                    str: '000302300D00040101',
+                    baseTagLength: 4,
+                    baseLengthTlvLength: 2,
+                    customByteLengthComputed: null
+                  }
+
+                  const expected ={ length: 2, differ: 6 }
+
+                 
+                  const result = JSON.stringify(createLengthParams(payload))
+ 
+
+                expect(result).to.deep.equal(JSON.stringify(expected));
+            });
+
+            it('should with custom length bytes', () => {
+                const payload = {
+                    baselengthByte: '81',
+                    i: 222,
+                    str: '00010101000203544C53000302300D0004010100050400004E20000604000007D000071F2F6C69622F7838365F36342D6C696E75782D676E752F6C6962746C732E736F0008132F7661722F6C6F672F766F6D6261742F746C7300090101000A0101000B0400000000000C0400000000000D81B600013C000101010002132F7661722F6C6F672F766F6D6261742F746C73000301010004010100050101000601010007010000080400000000000904000000000002240001010100020C2F6465762F747479555342300003093132372E302E302E310004022FB600030E000104000003E8000204000003E800043C0001010400020B000104000000010002010100020B000104000000020002010200020B000104000000030002010300020B0001040000000400020104',
+                    baseTagLength: 4,
+                    baseLengthTlvLength: 2,
+                    customByteLengthComputed: [
+                      {
+                        startByte: '81',
+                        nextByteLengthDefine: 2,
+                        iCounter: 6,
+                        differ: 8
+                      },
+                      {
+                        startByte: '82',
+                        nextByteLengthDefine: 4,
+                        iCounter: 6,
+                        differ: 10
+                      },
+                      {
+                        startByte: '83',
+                        nextByteLengthDefine: 6,
+                        iCounter: 6,
+                        differ: 12
+                      }
+                    ]
+                  }
+
+                const expected = { length: 364, differ: 8 }
+                 
+                const result = JSON.stringify(createLengthParams(payload))
+
+                expect(result).to.deep.equal(JSON.stringify(expected));
+            });
+
+
         });
+
+        describe('#makeToUpperCase', () => {
+            it('should makeToUpperCase', () => {
+                const str = "abc"
+                const expected = "ABC"
+                const result =  makeToUpperCase(str) 
+                expect(result).to.deep.equal(expected);
+            });
+        });
+
+        describe('#computeCustomByteLengths', () => {
+            it('should computeCustomByteLengths', () => {
+                const customByteLength = [
+                    { startByte: '81', nextByteLengthDefine: 1 },
+                    { startByte: '82', nextByteLengthDefine: 2 },
+                    { startByte: '83', nextByteLengthDefine: 3 }
+                  ]
+                const baseTagLength = 4
+                const result =  computeCustomByteLengths(customByteLength, baseTagLength) 
+                const expected = [
+                    { startByte: '81', nextByteLengthDefine: 2, iCounter: 6, differ: 8 },
+                    { startByte: '82', nextByteLengthDefine: 4, iCounter: 6, differ: 10 },
+                    { startByte: '83', nextByteLengthDefine: 6, iCounter: 6, differ: 12 }
+                  ]
+
+                expect(result).to.deep.equal(expected);
+            });
+        });
+
+        
 
   })
